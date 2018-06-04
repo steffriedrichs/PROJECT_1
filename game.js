cf2b4b6d4882a9e3eee378993661cd3f18ffe6e9
steffriedrichs <steffi.friedrichs@web.de>
2018-06-04 20:02:36 +0200

    basic HTML and create random array function

---
+++ game.js
@@ -0,0 +1,39 @@
+// game constructor
+  function Game(){
+  this.board = [];
+  this.stones = [];
+  this.n = 3;
+  this.score = 0;
+  }
+
+
+// function to initialize a new board or stone-set:
+Game.prototype.randomMatrix = function (n) {
+  var matrix = [];
+  // create empty matrix:
+  for(j = 0; j < n; j++){
+    var subMat = [];
+    for(i = 0; i < n; i++){
+      subMat.push(0);
+    } 
+    matrix.push(subMat);
+  }
+  // select x random fields to be marked: //var x = Math.round(n*n/2);  OR (first numbers)
+  var numFieldstoSelect = 2*n - 1;
+  // now select x random indices in the matrix:
+  var selectedFields = [];
+  while(selectedFields.length < numFieldstoSelect){
+    // create two random indices
+    var index = Math.floor(Math.random()*10)%n + '' + Math.floor(Math.random()*10)%n; 
+    if(!selectedFields.includes(index)){
+      selectedFields.push(index);
+    }  
+  }
+  // the selected indices will be set to 1 (= occupied)
+  for(j = 0; j < numFieldstoSelect; j++){
+    matrix[Number(selectedFields[j][0])][Number(selectedFields[j][1])] = 1;
+  }   
+  // the created matrix is returned
+  return matrix;
+}
+
---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+
+$(document).ready(function(){
+
+  // create new game:
+  var myGame = new Game(); 
+
+  // TO DO:  get n from user's choice
+  var n = 3;
+
+  myGame.board = randomMatrix(n);
+  myGame.stones = randomMatrix(n);
+
+  var html = '';
+  // memoryGame.cards.forEach(function (pic, index) {
+  //   html += '<div class= "card" id="card_' + pic.name + '">';
+  //   html += '<div class="back"';
+  //   html += '    name="'       + pic.img +  '">';
+  //   html += '</div>';
+  //   html += '<div class="front" ';
+  //   html += 'style="background: url(img/' + pic.img + ') no-repeat">';
+  //   html += '</div>';
+  //   html += '</div>';
+  // });
+
+// Add all the div's to the HTML
+  document.getElementById('memory_board').innerHTML = html;
+
+});  // end of document ready
