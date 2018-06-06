759b558de80697846f94a91d9eed792a72bd1889
steffriedrichs <steffi.friedrichs@web.de>
2018-06-06 18:46:22 +0200

    selection of row/col exists, event listener for stone moves

--- main.js
+++ main.js
@@ -9,8 +9,7 @@ $( "input" ).click(function() {
   myGame.board = myGame.randomMatrix(myGame.n);
   myGame.stones = myGame.randomMatrix(myGame.n);
 
-  //display Board in correct size:
-  //create correct number (n * n) of DOM elements 
+  //display Board: create correct number (n * n) of DOM elements 
   $("#gameBoard").empty();
   var countRow = 0;
   var countCol = 0;
@@ -19,7 +18,7 @@ $( "input" ).click(function() {
     $("#gameBoard").append('<div class="stone stone'+numFields+'"></div>');
     $(".stone.stone"+numFields).css({
       height:((100/myGame.n)/2)*3, 
-      width: ((100/myGame.n)/2)*3,  //size is ok.
+      width: ((100/myGame.n)/2)*3,     
       top:((100/myGame.n)/4 + 100/myGame.n*countRow)*3,
       left: ((100/myGame.n)/4 + 100/myGame.n*countCol)*3 
     }); 
@@ -29,7 +28,6 @@ $( "input" ).click(function() {
       countCol = 0;
     }
   }
-  
   //display marked fields in different color:
   var markedFields = '' + myGame.board;  
   markedFields = markedFields.split(','); //removes ',' between numbers!
@@ -39,7 +37,6 @@ $( "input" ).click(function() {
         $(".field.field"+f).toggleClass("marked"); // selector: ".oneclass.otherclass"
     }
   }
-
   //add stones to board - stones can be added to existing field divs as children
   var existingStones = '' + myGame.stones;  
   existingStones = existingStones.split(','); 
@@ -49,6 +46,87 @@ $( "input" ).click(function() {
     }
   }
 });
+
+var userSelection = 0;
+var isRow;
+document.addEventListener('keydown', function(e){
+  e.preventDefault();
+  // aswd for selection of the row or column
+  if( e.key === "a" ){
+    if(userSelection > 0){
+      userSelection--;
+    }else{
+      userSelection = (myGame.n-1);
+    }      
+    isRow = false;
+  }  
+  if( e.key === "d" ){   
+    if(userSelection < (myGame.n-1)){
+      userSelection++;
+    }else{
+      userSelection = 0;
+    }   
+    isRow = false;
+  }
+  if( e.key === "w" ){   
+    if(userSelection > 0){
+      userSelection--;
+    }else{
+      userSelection = (myGame.n-1);
+    }   
+    isRow = true;
+  }
+  if( e.key === "s" ){      
+    if(userSelection < (myGame.n-1)){
+      userSelection++;
+    }else{
+      userSelection = 0;
+    }
+    isRow = true;
+  }
+
+  var moveMade = false;
+  if( e.key === "ArrowLeft" ){      
+    myGame.makeMove(isRow, userSelection, -1);
+    moveMade = true;
+  }  
+  if( e.key === "ArrowRight" ){ 
+    myGame.makeMove(isRow, userSelection, 1);
+    moveMade = true;
+  }
+  if( e.key === "ArrowUp" ){   
+    myGame.makeMove(isRow, userSelection, 1);
+    moveMade = true;
+  }
+  if( e.key === "ArrowDown" ){      
+    myGame.makeMove(isRow, userSelection, -1);
+    moveMade = true;
+  }
+  // update stones on board 
+  // array of changed stone:
+  var changedStones = [];
+
+  // identify which stones have to be moved by number 'stone0'
+  if(moveMade==true){
+    for(var j = 1; j <= myGame.n; j++){
+      changedStones.push(j*(userSelection+1));  
+    }
+    console.log("These stones were moved: "+changedStones);
+  }else{
+    console.log("Nothing changed");
+  }
+
+  // das sollten wahrscheinlich besser zwei funktionen sein ... 
+});
+
+	
+
+// $( "#target" ).keydown(function() {
+//   alert( "Handler for .keydown() called." );
+// });
+
+// 
+
 
 
 // $( "rowSelector" ).click(function() {
@@ -60,9 +138,5 @@ $( "input" ).click(function() {
 //   $(this).addClass('on');
 //   $(this).prevAll().addClass('on');
 // });
-
-// Add all the div's to the HTML
-  //document.getElementById('memory_board').innerHTML = html;
-
 
 });  // end of document ready
