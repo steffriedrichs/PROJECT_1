3bf7962f78338bfd578333c250bc3c84fb797750
steffriedrichs <steffi.friedrichs@web.de>
2018-06-06 15:27:53 +0200

    stones are displayed correctly

--- main.js
+++ main.js
@@ -12,25 +12,43 @@ $( "input" ).click(function() {
   //display Board in correct size:
   //create correct number (n * n) of DOM elements 
   $("#gameBoard").empty();
+  var countRow = 0;
+  var countCol = 0;
   for(var numFields = 0; numFields < myGame.n * myGame.n; numFields++ ){
-    $("#gameBoard").append('<div class="field '+numFields+' fieldSize'+myGame.n+'x'+myGame.n+'"></div>');
+    $("#gameBoard").append('<div class="field field'+numFields+' fieldSize'+myGame.n+'x'+myGame.n+'"></div>');
+    $("#gameBoard").append('<div class="stone stone'+numFields+'"></div>');
+    $(".stone.stone"+numFields).css({
+      height:((100/myGame.n)/2)*3, 
+      width: ((100/myGame.n)/2)*3,  //size is ok.
+      top:((100/myGame.n)/4 + 100/myGame.n*countRow)*3,
+      left: ((100/myGame.n)/4 + 100/myGame.n*countCol)*3 
+    }); 
+    countCol++;
+    if(countCol%myGame.n == 0){
+      countRow++;
+      countCol = 0;
+    }
   }
-
+  
   //display marked fields in different color:
   var markedFields = '' + myGame.board;  
   markedFields = markedFields.split(','); //removes ',' between numbers!
   for(var f = 0; f < markedFields.length; f++){
     if(markedFields[f] == 1){
         //change color of corresponding div - toggle marked class
-        $(".field."+f).toggleClass("marked"); // selector: ".oneclass.otherclass"
+        $(".field.field"+f).toggleClass("marked"); // selector: ".oneclass.otherclass"
+    }
+  }
+
+  //add stones to board - stones can be added to existing field divs as children
+  var existingStones = '' + myGame.stones;  
+  existingStones = existingStones.split(','); 
+  for(var s = 0; s < existingStones.length; s++){
+    if(existingStones[s] == 0){
+       $(".stone.stone"+s).toggleClass("hidden"); 
     }
   }
 });
-
-
-// $("rowSelector").click(function() {
-//   $( this ).toggleClass("inactive");
-// });
 
 
 // $( "rowSelector" ).click(function() {
